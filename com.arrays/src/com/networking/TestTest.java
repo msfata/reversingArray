8e8b181d815ff24ece93595a3e2d72dae2bf90df
44742 <44742@ProgramminHelp>
2019-04-23 18:26:14 +0100

    updated

---
+++ com.arrays/src/com/networking/TestTest.java
@@ -0,0 +1,48 @@
+package com.networking;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.test.Calculation;
+/*
+ * by msfata programming help 
+ * */
+class TestTest {
+
+	@Test
+	void additionTest() {
+		int[] a = { 1, 2, 51 };
+		Calculation c = new Calculation();
+		assertEquals(c.add(a), 54);
+	}
+
+	@Test
+	void deductionTest() {
+		int[] a = { 1, 2, 51 };
+		Calculation c = new Calculation();
+		assertEquals(c.deduct(a), -54);
+	}
+
+	@Test
+	void divisionTest() {
+		int[] a = { 1, 2, 51 };
+		Calculation c = new Calculation();
+		assertEquals(c.devide(2, a), 27);
+	}
+
+	@Test
+	void multiplicationTest() {
+		int[] a = { 1, 2, 51 };
+		Calculation c = new Calculation();
+		assertEquals(c.multiply(2, a), 108);
+	}
+
+	@Test
+	void modulusTest() {
+		int[] a = { 1, 2, 51 };
+		Calculation c = new Calculation();
+		assertEquals(c.modulus(2, a), 0);
+	}
+
+}
